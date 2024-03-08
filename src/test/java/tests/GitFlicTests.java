81c9b624c2e96331420966250a00737ebd887570
RenatAbdullin <arn_freeman@mail.ru>
2024-03-09 00:03:57 +0500

    add new test

---
+++ src/test/java/tests/GitFlicTests.java
@@ -0,0 +1,48 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebElementCondition;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class GitFlicTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void createNewJavaProjectTest() {
+        open("https://gitflic.ru/");
+        $("[data-barba-prevent=self]").click();
+        $("#email").setValue("testuser.freeman@gmail.com");
+        $("#password").setValue("user91test24");
+        $("[type=submit]").click();
+        $("a[href='/project']").click();
+        $("span[class='d-none d-md-block']").click();
+        $("#title").setValue("My new project");
+        $("#vs1__combobox").click();
+        $(".vs__selected-options").setValue("Java");
+        //$("[button type='submit']").click();
+        sleep(5000);
+
+    }
+//    @Test
+//    void createNewTeamTest() {
+//        open("https://gitflic.ru/");
+//        $("[data-barba-prevent=self]").click();
+//        $("#email").setValue("testuser.freeman@gmail.com");
+//        $("#password").setValue("user91test24");
+//        $("[type=submit]").click();
+//        $("a[href='/team']").click();
+//        $("span[class='d-none d-md-block']").click();
+//        $("#title").setValue("My new team 1");
+//        $("#description").setValue("Team new my");
+//        $("[button type='submit']").click();
+//        sleep(3000);
+//
+//    }
+}
