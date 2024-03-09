9eae4a58e095e8b558ccd37ea88b96933748c85f
RenatAbdullin <arn_freeman@mail.ru>
2024-03-10 00:39:10 +0500

    add new test

--- src/test/java/tests/GitFlicTests.java
+++ src/test/java/tests/GitFlicTests.java
@@ -1,13 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebElementCondition;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GitFlicTests {
@@ -16,40 +12,52 @@ public class GitFlicTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://gitflic.ru";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 20000;
+        Configuration.timeout = 10000;
     }
 
     @Test
     void createNewJavaProjectTest() {
         open("/auth/login");
         //$("[data-barba-prevent=self]").click();
+
+        //----логинимся
+
         $("#email").setValue("testuser.freeman@gmail.com");
         $("#password").setValue("user91test24");
         $("[type=submit]").click();
+
         open("/project/testuserfreeman/my-new-project");
+
+
+        //----создаем новый проект
+
         //$("a[href='/project']").click();
 //        $("span[class*='d-none d-md-block']").click();
 //        $("#title").setValue("My new project");
+
+        //----выбираем язык программирования
 
         //$("#vs1__combobox").click();
         //$("[input.vs__search]").setValue("Java").click();
         //$(".vs__selected-options").setValue("Java");
 
-
         //$("button.btn-success").click();
+
+        //----проверяем результат
 //        $("div[class*='project__settings-links']").shouldHave(text("testuserfreeman" + " / " + "My new project"));
-//
+
+
+
+        //-----удаляем новый проект
+
         $("a[href='/project/testuserfreeman/my-new-project/setting']").click();
         $("button[data-target='deleteProject']").click();
 
-        //<input name="controlString"
 
-        //modal-footer d-flex justify-content-center text-break
-
+        $("input[class*='form-control-sm']").click();
 
 
-        $("input[name='controlString']").click();
-//       //$("input[class*='form-control-sm']").setValue("testuserfreeman/my-new-project");
+                //setValue("testuserfreeman/my-new-project");
 //        $("div input[name=controlString]").setValue("testuserfreeman/my-new-project");
 //        sleep(4000);
 //        $$("div.modal-footer").findBy(text("Удалить")).click();
